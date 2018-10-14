0a703324490ef6aec7f92e2fe16f0a8f0f0ad390
Stoyan Delev <s.delev@elements.nl>
2018-10-14 09:24:47 +0300

    remove unused assets

--- ui/utils/api.js
+++ ui/utils/api.js
@@ -1,8 +1,7 @@
 import { map } from "lodash";
 
 const config = {
-  //API_URL: process.env.API_URL
-  API_URL: "http://localhost:3000/api"
+  API_URL: process.env.API_URL
 };
 
 const toQueryString = obj =>
